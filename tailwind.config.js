280890d8c68851489ed05445ab723b1f2cce970f
b7c8p <morapaezc@gmail.com>
2022-04-13 21:39:07 -0500

    add route modals to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,7 @@ module.exports = {
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
     './container/**/*.{js,ts,jsx,tsx}',
+    './modals/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {},
